ab3cfea50c32c4f586bb6ff9b5496d14f076e905
goporo <goporolo1@gmail.com>
2022-10-31 20:56:12 +0700

    update courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,5 +1,5 @@
 const Course = require('../models/Course')
-const { multipleMongooseToObject } = require('../../util/mongoose')
+const { multipleMongooseToObject, mongooseToObject } = require('../../util/mongoose')
 
 
 class CourseController {
@@ -7,9 +7,43 @@ class CourseController {
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
             .then(course => {
-                res.render('courses/show');
+                res.render('courses/show', { course: mongooseToObject(course) });
             })
             .catch(next);
+    }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://i3.ytimg.com/vi/${req.body.videoId}/maxresdefault.jpg`;
+        const course = new Course(formData);
+        course.save()
+            .then(() => res.redirect('/'))
+            .catch((e) => {
+                console.log(e)
+            })
+
+    }
+
+    // [GET] /courses/:id/edit
+    edit(req, res, next) {
+        Course.findById(req.params.id)
+            .then(course => res.render('courses/edit', {
+                course: mongooseToObject(course)
+            }))
+            .catch(next)
+
+    }
+    //[PUT] /courses/:id
+    update(req, res, next) {
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next)
     }
 }
 
